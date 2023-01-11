b976deefbd6bd668cfbccca65037b95b3639be40
Luis Gustavo <luguin444@gmail.com>
2023-01-11 20:58:45 -0300

    update interface

--- supervisorioHandler.py
+++ supervisorioHandler.py
@@ -53,30 +53,30 @@ class SupervisorioHandler(BoxLayout):
 
     def read_data_from_CLP(self):
         self._real_time_data['timestamp'] = datetime.now()
-        self._real_time_data['temperatura_enrolamento_r'] = self.lerFloat(
-            700) * 0.1
-        self._real_time_data['temperatura_enrolamento_s'] = self.lerFloat(
-            702) * 0.1
-        self._real_time_data['temperatura_enrolamento_t'] = self.lerFloat(
-            704) * 0.1
-        self._real_time_data['temperatura_carcaca'] = self.lerFloat(
-            706) * 0.1
-        self._real_time_data['frequencia_motor_rpm'] = self.lerFloat(
-            884)
-        self._real_time_data['torque_ventilador_radial'] = self.lerFloat(
-            1422)
-        self._real_time_data['torque_ventilador_axial'] = self.lerFloat(
-            1424)
-        self._real_time_data['vazao_ar'] = self.lerFloat(
-            714)
-        self._real_time_data['velocidade_ar'] = self.lerFloat(
-            712)
-        self._real_time_data['temperatura_ar'] = self.lerFloat(
-            710)
-        self._real_time_data['temperatura_tubo_azul'] = self.lerFloat(
-            1220) * 0.1
-        self._real_time_data['temperatura_tubo_vermelho'] = self.lerFloat(
-            1218) * 0.1
+        self._real_time_data['temperatura_enrolamento_r'] = round(self.lerFloat(
+            700) * 0.1, 2)
+        self._real_time_data['temperatura_enrolamento_s'] = round(self.lerFloat(
+            702) * 0.1, 2)
+        self._real_time_data['temperatura_enrolamento_t'] = round(self.lerFloat(
+            704) * 0.1, 2)
+        self._real_time_data['temperatura_carcaca'] = round(self.lerFloat(
+            706) * 0.1, 2)
+        self._real_time_data['frequencia_motor_rpm'] = round(self.lerFloat(
+            884), 2)
+        self._real_time_data['torque_ventilador_radial'] = round(self.lerFloat(
+            1422), 2)
+        self._real_time_data['torque_ventilador_axial'] = round(self.lerFloat(
+            1424), 2)
+        self._real_time_data['vazao_ar'] = round(self.lerFloat(
+            714), 2)
+        self._real_time_data['velocidade_ar'] = round(self.lerFloat(
+            712), 2)
+        self._real_time_data['temperatura_ar'] = round(self.lerFloat(
+            710), 2)
+        self._real_time_data['temperatura_tubo_azul'] = round(self.lerFloat(
+            1220) * 0.1, 2)
+        self._real_time_data['temperatura_tubo_vermelho'] = round(self.lerFloat(
+            1218) * 0.1, 2)
 
     def lerFloat(self, addr):
         num_float = self._modbus_client.read_holding_registers(addr, 2)
@@ -89,6 +89,29 @@ class SupervisorioHandler(BoxLayout):
     def update_GUI(self):
         self.ids.temp_enrolamento_r.text = str(
             self._real_time_data['temperatura_enrolamento_r'])
+        self.ids.temp_enrolamento_s.text = str(
+            self._real_time_data['temperatura_enrolamento_s'])
+        self.ids.temp_enrolamento_t.text = str(
+            self._real_time_data['temperatura_enrolamento_t'])
+        self.ids.temp_carcaca.text = str(
+            self._real_time_data['temperatura_carcaca'])
+        self.ids.freq_motor_rpm.text = str(
+            self._real_time_data['frequencia_motor_rpm'])
+        self.ids.torque_vent_radial.text = str(
+            self._real_time_data['torque_ventilador_radial'])
+        self.ids.vel_ar.text = str(
+            self._real_time_data['velocidade_ar'])
+        self.ids.temp_ar.text = str(
+            self._real_time_data['temperatura_ar'])
+        self.ids.temp_tub_azul.text = str(
+            self._real_time_data['temperatura_tubo_azul'])
+        self.ids.temp_tub_vermelho.text = str(
+            self._real_time_data['temperatura_tubo_vermelho'])
+
+        # self.ids.freq_motor_rpm.text = str(
+        #     self._real_time_data['torque_ventilador_axial'])
+        # self.ids.freq_motor_rpm.text = str(
+        #     self._real_time_data['vazao_ar'])
 
     def insert_data_to_database(self):
         pass
